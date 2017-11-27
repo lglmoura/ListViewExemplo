a6a6ae2052914abeb14a4d26d85d067260da819e
Luiz Gustavo <lglmoura@gmail.com>
2017-11-27 19:23:55 -0200

    Criando a classe de Model Escola

---
+++ app/src/main/java/br/edu/iff/pooa20172/listviewexemplo/Escola.java
@@ -0,0 +1,42 @@
+package br.edu.iff.pooa20172.listviewexemplo;
+
+/**
+ * Created by lglmo on 27/11/2017.
+ */
+
+public class Escola {
+
+    private String nome;
+    private int imagem; // vai armazenar o identificador do recurso
+    private String endereco;
+
+    public Escola(String nome, int imagem, String endereco) {
+        this.nome = nome;
+        this.imagem = imagem;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
