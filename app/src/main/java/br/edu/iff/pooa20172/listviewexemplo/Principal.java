4ec09d9052aab6d299aa65fa851c8482c2f35dc4
Luiz Gustavo <lglmoura@gmail.com>
2017-11-27 19:18:37 -0200

    Criando o Layout do Item

--- app/src/main/java/br/edu/iff/pooa20172/listviewexemplo/Principal.java
+++ app/src/main/java/br/edu/iff/pooa20172/listviewexemplo/Principal.java
@@ -2,6 +2,8 @@ package br.edu.iff.pooa20172.listviewexemplo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Principal extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        ListView lista = (ListView) findViewById(R.id.lvEquipes);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.equipes,android.R.layout.simple_list_item_1);
+
+        lista.setAdapter(adapter);
     }
 }
